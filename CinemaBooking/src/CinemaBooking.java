f37baecc089f60c2951f6784129f8a05ddcaa212
Gabby202 <gabbu202@gmail.com>
2017-02-10 12:19:53 +0000

    + Finished Movie Card

--- CinemaBooking/src/CinemaBooking.java
+++ CinemaBooking/src/CinemaBooking.java
@@ -18,7 +18,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
@@ -36,8 +35,8 @@ public class CinemaBooking extends JFrame implements ActionListener, MouseListen
 	JButton backButton;
 
 	// movie list method arrays made global to give mouse listener methods scope
-	JPanel moviePanel[];
-	JLabel movieLabel[];
+	JPanel moviePanel[], spacePanel[], descriptionPanel[];
+	JLabel movieLabel[], movieDescription[];
 	Icon movieImage[];
 
 
@@ -159,34 +158,51 @@ public class CinemaBooking extends JFrame implements ActionListener, MouseListen
 		moviePanel = new JPanel[4];
 		movieLabel = new JLabel[4];
 		movieImage = new ImageIcon[4];
-		JPanel spacePanel[] = new JPanel[4];
+		spacePanel = new JPanel[4];
+		descriptionPanel = new JPanel[4];
+		movieDescription = new JLabel[4];
 		movieList.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 		
 		movieImage[0] = new ImageIcon("src/images/the_hobbit1.jpg");
 		movieImage[1] = new ImageIcon("src/images/captain_america1.jpg");
 		movieImage[2] = new ImageIcon("src/images/hunger_games1.jpg");
 		movieImage[3] = new ImageIcon("src/images/star_wars1.jpg");
-
 		
+		movieDescription[0] = new JLabel("The Hobbit: The Desolation Of Smaug");
+		movieDescription[1] = new JLabel("Captain America: Civil War");
+		movieDescription[2] = new JLabel("The Hunger Games: Mockingjay");
+		movieDescription[3] = new JLabel("Star Wars: The Force Awakens");
+		
+
 		for (int i = 0; i < 4; i++) {
 			moviePanel[i] = new JPanel();
 			movieLabel[i] = new JLabel(movieImage[i]);
 			spacePanel[i] = new JPanel();	
+			descriptionPanel[i] = new JPanel();
+			
 			movieLabel[i].setHorizontalAlignment(JLabel.LEFT);
 			moviePanel[i].setLayout(new BorderLayout());
-			spacePanel[i].setPreferredSize(new Dimension(30, 0));
-			moviePanel[i].add(spacePanel[i], BorderLayout.WEST);
-			moviePanel[i].add(movieLabel[i], BorderLayout.CENTER);
-			
+			spacePanel[i].setPreferredSize(new Dimension(68, 0));
+			descriptionPanel[i].setPreferredSize(new Dimension(538, 0));
+			descriptionPanel[i].setLayout(new BorderLayout());
+			descriptionPanel[i].setBackground(new Color(107, 106, 104));
+			movieDescription[i].setForeground(Color.WHITE);
+			movieDescription[i].setFont(new Font("Candara", Font.BOLD, 24));
+		
 			movieList.add(moviePanel[i]);
 			spacePanel[i].setBackground(new Color(107, 106, 104));
 			moviePanel[i].setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 			moviePanel[i].setBackground(new Color(107, 106, 104));
 			moviePanel[i].addMouseListener(this);
+			descriptionPanel[i].add(movieDescription[i], BorderLayout.WEST);
+			movieDescription[i].setHorizontalAlignment(JLabel.LEFT);
+
+			moviePanel[i].add(spacePanel[i], BorderLayout.WEST);
+			moviePanel[i].add(movieLabel[i], BorderLayout.CENTER);
+			moviePanel[i]. add(descriptionPanel[i], BorderLayout.EAST);
+			
 		}
 		
-		
-
 		return movieList;
 	}
 
@@ -220,26 +236,49 @@ public class CinemaBooking extends JFrame implements ActionListener, MouseListen
 		// change background color of selected panel in movie list
 		if (e.getSource() == moviePanel[0]) {
 			moviePanel[0].setBackground(new Color(148, 146, 143));
+			spacePanel[0].setBackground(new Color(148, 146, 143));
+			descriptionPanel[0].setBackground(new Color(148, 146, 143));
+
+
 		}else{
 			moviePanel[0].setBackground(new Color(107, 106, 104));
+			spacePanel[0].setBackground(new Color(107, 106, 104));
+			descriptionPanel[0].setBackground(new Color(107, 106, 104));
+
 		}
 		
 		if(e.getSource() == moviePanel[1]){
 			moviePanel[1].setBackground(new Color(148, 146, 143));
+			spacePanel[1].setBackground(new Color(148, 146, 143));
+			descriptionPanel[1].setBackground(new Color(148, 146, 143));
+
 		}else{
 			moviePanel[1].setBackground(new Color(107, 106, 104));
+			spacePanel[1].setBackground(new Color(107, 106, 104));
+			descriptionPanel[1].setBackground(new Color(107, 106, 104));
 		}
 		
 		if (e.getSource() == moviePanel[2]) {
 			moviePanel[2].setBackground(new Color(148, 146, 143));
+			spacePanel[2].setBackground(new Color(148, 146, 143));
+			descriptionPanel[2].setBackground(new Color(148, 146, 143));
+
 		}else{
 			moviePanel[2].setBackground(new Color(107, 106, 104));
+			spacePanel[2].setBackground(new Color(107, 106, 104));
+			descriptionPanel[2].setBackground(new Color(107, 106, 104));
+
 		}
 		
 		if(e.getSource() == moviePanel[3]){
 			moviePanel[3].setBackground(new Color(148, 146, 143));
+			spacePanel[3].setBackground(new Color(148, 146, 143));
+			descriptionPanel[3].setBackground(new Color(148, 146, 143));
+
 		}else{
 			moviePanel[3].setBackground(new Color(107, 106, 104));
+			spacePanel[3].setBackground(new Color(107, 106, 104));
+			descriptionPanel[3].setBackground(new Color(107, 106, 104));
 		}
 		
 		
