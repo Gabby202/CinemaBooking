4bbb9f7a6c84654815d878d2167c56ae57fc32a8
Professor-Periwinkle <B00084811@student.itb.ie>
2017-03-23 13:36:20 +0000

    Completed DBConnection class Template

--- CinemaBooking/src/DBConnection.java
+++ CinemaBooking/src/DBConnection.java
@@ -1,43 +1,65 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
+
+import javax.swing.Icon;
 
 class DBConnection {
 
+	//DB conection info
 	private String DBUrl = "jdbc:mysql://localhost:port/databasename";
 	private String DBUser = "myuser";
 	private String DBPass = "1234";
 
 	private DBConnection() {
-		
-		try (
-			Connection conn = DriverManager.getConnection(DBUrl, DBUser, DBPass);
-
-			Statement stmt = conn.createStatement();) {
-			String strSelect = "select title, price, qty from books";
-			System.out.println("The SQL query is: " + strSelect + "\n");
-
-			ResultSet rset = stmt.executeQuery(strSelect);
-
-			System.out.println("The records selected are:");
-			int rowCount = 0;
-			while (rset.next()) {
-				String title = rset.getString("title");
-				double price = rset.getDouble("price");
-				int qty = rset.getInt("qty");
-				System.out.println(title + ", " + price + ", " + qty);
-				++rowCount;
-			}
-			System.out.println("Total number of records = " + rowCount);
-
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+		try{
+			//DB connection here
+		}catch(SQLException e){
+			
 		}
 	}
 	
-	public static void main(String[] args) {
-		new DBConnection();
+	//All methods might be returning something depending on its purpose
+	
+	//All methods will either return something or throw an exception for errors depending how we want to deal with the error
+	//For if we want to how an error somewhere we will need to throw an error, but some errors can be dealt within this class
+
+	
+	public static void Login(String userName, char[] password) {
+		//Attempts to confirm login details
+		//Returns account type, or null for an error
+	}
+
+	public static void Register(String userName, char[] pass, String name, String email) {
+		//Attempts to register a new user
+		//Returns if failed or not
+	}
+
+	public static void getMovies() {
+		//Returns the list of currant movies showing with their posters and descriptions
+
+	}
+
+	public static void setMvoies(String title[], Icon img[], String desp) {
+		//Attempts to set what movies are currently showing, set the poster for the movie, and the description 
+
+	}
+	
+	public static void getTimes(String movie){
+		//Attempts to get the times for selected movie
+	}
+	
+	public static void setTimes(String movie, Date times){
+		//Attempts to set the times for selected movie
+	}
+	
+	public static void getSeats(String movie, Date time){
+		//Returns to the seating data for a movie at a certain date
+	}
+	
+	public static void setSeat(int row, int col, int state){
+		//Attempts to set the state of a selected seat
+		//i.e. take or not
 	}
 }
